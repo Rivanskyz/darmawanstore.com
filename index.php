603c7cbfac0da6b16fc40b4a0054c0160bebe9cc
rivanskyz <rivanskyz@gmail.com>
2015-04-26 21:49:31 +0700

    Commit Pertama, Versi 1

---
+++ index.php
@@ -0,0 +1,57 @@
+<html>
+	<head>
+		<title>
+		</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	</head>
+	
+	<body>
+	<div class="wrap">
+		<div class="topbar">
+			<div class="menubar">
+			<a href="payment.php">Payment</a> | 
+			<a href="payment.php">Testiminy</a> | 
+			<a href="payment.php">Contact</a>
+			</div>
+			<div class="search">
+				<form method="get" action="search.php">
+					<input type="text" name="searchbox" placeholder="Search"/><input type="submit" name="search" value="Serach"/> 
+				<form>
+			</div>
+			
+		</div>
+		
+		<div class="sidebar">
+			
+			<div class="title">
+			<img src="img/ico/home.png"/>DarmawanStore
+			</div>
+			
+			<div class="category">
+			sidebar<br>
+			sidebar
+			</div>
+		</div>
+		<div class="main">
+			
+			<div class="content">
+				<div class="box">
+					<div class="title">
+						facebook
+					</div>
+					<div class="pict">
+						<img src="img/1.png"/>
+					</div>
+					<div class="price">
+					Rp. 999.999,-
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		<div class="footer">
+			&copyDarmawanSoftware
+		</div>
+	</div>	
+	</body>
+</html>
